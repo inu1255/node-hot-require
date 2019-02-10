841e637f3df91f776a2d7cf6b440a0a1d3b8648e
inu1255 <929909260@qq.com>
2019-02-10 23:31:45 +0800

    use native proxy,fix dumplicate require bug

--- index.js
+++ index.js
@@ -6,9 +6,8 @@
 
 const callsites = require("callsites");
 const path = require("path");
-const Proxy = require("node-module-proxy");
-const events = require('events');
-const chokidar = require('chokidar');
+const events = require("events");
+const chokidar = require("chokidar");
 
 const emitter = new events.EventEmitter();
 const _proxys = {}; // module 代理
@@ -26,13 +25,17 @@ function dfs(data, fn) {
     if (fn(data)) dfs(data.children, fn);
 }
 
+/**
+ * 获取后代模块
+ * @param {string} filename
+ */
 function cacheChild(filename) {
     var mod = require.cache[filename];
-    const cache = [];
+    const cache = {};
     dfs(mod.children, function(item) {
-        if (typeof emitter.filter==="function" && !emitter.filter(item.filename)) return false;
+        if (typeof emitter.filter === "function" && !emitter.filter(item.filename)) return false;
         if (item.filename.indexOf("node_modules") < 0) {
-            cache.push(item);
+            cache[item.filename] = item;
             return true;
         }
     });
@@ -41,50 +44,57 @@ function cacheChild(filename) {
 
 /**
  * 清空module cache 返回 roolback
- * @param {string} filename 
+ * @param {string[]} filenames
  */
-function cleanCache(filename) {
-    var mod = require.cache[filename];
-    if (!mod)
-        return function() {};
-    // remove reference in module.parent
-    if (mod.parent) {
-        mod.parent.children.splice(mod.parent.children.indexOf(mod), 1);
-    }
-    // 清空child module
-    const children = _cache_children[filename];
-    for (let item of children) {
-        require.cache[item.filename] = null;
-    }
-    require.cache[filename] = null;
-    // 还原 cache
-    return function rollback() {
-        for (let item of children) {
-            require.cache[item.filename] = item;
+function cleanCache(filenames, trigger) {
+    var mods = [];
+    var children = [];
+    for (let filename of filenames) {
+        var mod = require.cache[filename];
+        if (!mod) continue;
+        const cache = _cache_children[filename];
+        if (trigger && !(cache[trigger] || filename == trigger)) continue;
+        // remove reference in module.parent
+        if (mod.parent) {
+            mod.parent.children.splice(mod.parent.children.indexOf(mod), 1);
         }
-        require.cache[filename] = mod;
-        mod.parent.children.push(mod);
-    };
+        // 清空child module
+        for (let filename in cache) {
+            require.cache[filename] = null;
+            children.push(cache[filename]);
+        }
+        require.cache[filename] = null;
+        mods.push(mod);
+    }
+    return { mods, children };
 }
 
-/**
- * 重新require一个module
- * @param {string} filename 
- */
-function reload(filename, errFn) {
-    const rollback = cleanCache(filename);
-    try {
-        const mod = require(filename);
-        _cache_children[filename] = cacheChild(filename);
-        if (watchAll.watcher) emitter.watchAll();
-        return mod;
-    } catch (err) {
-        rollback();
-        if (errFn) {
-            errFn(err);
+function proxyIt(it) {
+    return {
+        value: new Proxy(it, {
+            get(_, p) {
+                return it[p];
+            },
+            set(_, p, value) {
+                return (it[p] = value);
+            },
+            apply(_, thisArg, args) {
+                return it.apply(thisArg, args);
+            },
+            construct(_, args) {
+                new it(...args);
+            },
+            ownKeys(_) {
+                return Object.keys(it);
+            },
+            enumerate(_) {
+                return Object.keys(it);
+            }
+        }),
+        use(m) {
+            it = m;
         }
-    }
-    return require.cache[filename];
+    };
 }
 
 emitter.require = function(modulePath) {
@@ -97,16 +107,16 @@ emitter.require = function(modulePath) {
     _cache_children[filename] = cacheChild(filename);
     var proxy = _proxys[filename];
     if (proxy) return proxy.value;
-    proxy = new Proxy(mod);
+    proxy = proxyIt(mod);
     _proxys[filename] = proxy;
     return proxy.value;
 };
 
 function watchAll() {
     if (!watchAll.watcher) {
-        watchAll.watcher = chokidar.watch('.zzzq');
-        watchAll.watcher.on("change", function() {
-            emitter.reloadAll();
+        watchAll.watcher = chokidar.watch(".zzzq");
+        watchAll.watcher.on("change", function(filename) {
+            emitter.reloadAll(filename);
         });
     }
     for (let filename in _proxys) {
@@ -114,24 +124,41 @@ function watchAll() {
             // console.log("watch", filename);
             watchAll.watcher.add(filename);
         }
-        for (let item of _cache_children[filename]) {
-            if (!_file_watchers[item.filename]) {
-                // console.log("watch", item.filename);
-                watchAll.watcher.add(item.filename);
+        for (let name in _cache_children[filename]) {
+            if (!_file_watchers[name]) {
+                // console.log("watch", name);
+                watchAll.watcher.add(name);
             }
         }
     }
 }
 
-emitter.reloadAll = function() {
+/**
+ * 重载module,重载所有依赖trigger的hot.require的模块
+ * @param {string} [trigger] 触发文件,不传则更新全部
+ */
+emitter.reloadAll = function(trigger) {
     var error;
-    for (let filename in _proxys) {
-        let proxy = _proxys[filename];
-        let mod = reload(filename, function(err) {
-            emitter.emit("error", err);
-            error = err;
-        });
-        proxy.use(mod);
+    var filenames = Object.keys(_proxys);
+    const { mods, children } = cleanCache(filenames, trigger);
+    try {
+        for (let item of mods) {
+            let filename = item.filename;
+            const mod = require(filename);
+            _cache_children[filename] = cacheChild(filename);
+            if (watchAll.watcher) emitter.watchAll();
+            let proxy = _proxys[filename];
+            proxy.use(mod);
+        }
+    } catch (err) {
+        for (let item of children) {
+            require.cache[item.filename] = item;
+        }
+        for (let item of mods) {
+            require.cache[item.filename] = item;
+            item.parent.children.push(item);
+        }
+        emitter.emit("error", err);
     }
     emitter.emit("reload", error);
     return error;
--- test/test.js
+++ test/test.js
@@ -12,14 +12,14 @@ const hot = require("../index");
 const fs = require("fs");
 const path = require("path");
 
-filename = path.join(__dirname,"./s.js");
-fs.writeFileSync(filename, "module.exports='abc'");
+var filename = path.join(__dirname,"./s.js");
+fs.writeFileSync(filename, "module.exports=()=>'abc'");
 
 const s = hot.require("./s.js");
 
 console.log(s());
 
-fs.writeFileSync(filename, "module.exports='123'");
+fs.writeFileSync(filename, "module.exports=()=>'123'");
 
 hot.reloadAll();
 
